c498d66734abc142a587d1ea53bf4f3837e563b1
Abelit <ychenid@live.com>
2019-05-06 18:01:19 +0800

    add display property for page

--- frontend/src/views/error/NotPermission.vue
+++ frontend/src/views/error/NotPermission.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-container fill-height class="grey" fluid>
+    <v-container fill-height  fluid :style="'display:' + display" class="loading-overlay">
         <v-layout justify-center align-center>
           <v-flex shrink>
             <p>dfadfa</p>
@@ -7,3 +7,11 @@
         </v-layout>
       </v-container>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    display: ""
+  })
+}
+</script>
