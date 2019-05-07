2975cf009a3348e15e514e1ee484e761024c23f4
Abelit <ychenid@live.com>
2019-05-08 01:49:48 +0800

    add access page for prohibit someone who has not permission to get the page

--- frontend/src/views/dashboard/FinanceDash.vue
+++ frontend/src/views/dashboard/FinanceDash.vue
@@ -2,12 +2,12 @@
   <v-container grid-list-xl fluid>
     <v-layout row wrap>
       <v-flex>
-      <h1>Finance Dashboard</h1>
-      <table-demo></table-demo>
-      <table-two-demo></table-two-demo>
-      <table-one-demo></table-one-demo>
-      <table-three-demo></table-three-demo>
-    </v-flex>
+        <h1>Finance Dashboard</h1>
+        <table-demo></table-demo>
+        <table-two-demo></table-two-demo>
+        <table-one-demo></table-one-demo>
+        <table-three-demo></table-three-demo>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -17,12 +17,11 @@ import TableDemo from "@/components/tables/TableDemo";
 import TableTwoDemo from "@/components/tables/TableTwoDemo";
 import TableOneDemo from "@/components/tables/TableOneDemo";
 import TableThreeDemo from "@/components/tables/TableThreeDemo";
-import {mapState} from "vuex";
+import { mapState } from "vuex";
 
 export default {
   name: "Report",
-  data: () => ({
-  }),
+  data: () => ({}),
   components: {
     TableDemo,
     TableTwoDemo,
@@ -30,21 +29,21 @@ export default {
     TableThreeDemo
   },
   computed: {
-    ...mapState(["windowSize"]),
+    ...mapState(["windowSize"])
   },
-  mounted() {
-    
-  }
+  mounted() {}
 };
 </script>
 
 
 <style lang="stylus">
-  .setting-fab 
-    top:50%!important; 
-    right:0;
-    border-radius:0  
-  .page-wrapper
-    min-height:calc(100vh - 64px - 50px - 81px );  
+.setting-fab {
+  top: 50% !important;
+  right: 0;
+  border-radius: 0;
+}
 
+.page-wrapper {
+  min-height: calc(100vh - 64px - 50px - 81px);
+}
 </style>
--- frontend/src/views/error/NotPermission.vue
+++ frontend/src/views/error/NotPermission.vue
@@ -1,13 +1,18 @@
 <template>
-    <v-container fill-height  fluid  class="grey" style="height: 89vh;z-index: 0">
-        <v-layout justify-center align-center>
-          <v-flex text-xs-center shrink>
-            <v-alert :value="true" type="warning" class="mover">
-              Sorry! No permission to access this page!
-            </v-alert>
-          </v-flex>
-        </v-layout>
-      </v-container>
+  <v-container fill-height fluid style="height: 89vh">
+    <v-layout justify-center align-center>
+      <v-flex text-xs-center shrink>
+        <v-alert
+          :value="true"
+          type="warning"
+          class="mover"
+        >Sorry! No permission to access this page!</v-alert>
+      </v-flex>
+    </v-layout>
+    <v-layout justify-center align-center class="d-forbidden-page">
+      <v-flex text-xs-center shrink></v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -15,17 +20,47 @@ export default {
   data: () => ({
     display: ""
   })
-}
+};
 </script>
 
 <style scoped>
 .mover {
-  z-index: 0;
-  animation: move 4s linear infinite alternate; 
+  z-index: 1;
+  animation: move 4s linear infinite alternate;
 }
 
 @keyframes move {
-  from { z-index: 0; transform: scale(1); }
-  to { z-index: 4; transform: scale(2.5); }
+  from {
+    z-index: 1;
+    transform: scale(1);
+  }
+  to {
+    z-index: 3;
+    transform: scale(1.5);
+  }
+}
+
+.d-forbidden-page {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  -webkit-filter: blur(1px);
+  filter: blur(1px);
+  animation: testmove 4s linear infinite alternate;
+}
+
+@keyframes testmove {
+  from {
+    -webkit-filter: blur(1px);
+    filter: blur(1px);
+    background-color: #49424231;
+  }
+  to {
+    -webkit-filter: blur(5px);
+    filter: blur(5px);
+    background-color: #242121c4;
+  }
 }
 </style>
