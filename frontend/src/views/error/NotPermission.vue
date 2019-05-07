2be9d511ce06326b06a957b91f29b85c776e96eb
abelit <ychenid@live.com>
2019-05-07 18:02:27 +0800

    update NotPermission component and update its style

--- frontend/src/views/error/NotPermission.vue
+++ frontend/src/views/error/NotPermission.vue
@@ -1,8 +1,10 @@
 <template>
-    <v-container fill-height  fluid :style="'display:' + display" class="loading-overlay">
+    <v-container fill-height  fluid  class="grey" style="height: 89vh;z-index: 0">
         <v-layout justify-center align-center>
-          <v-flex shrink>
-            <p>dfadfa</p>
+          <v-flex text-xs-center shrink>
+            <v-alert :value="true" type="warning" class="mover">
+              Sorry! No permission to access this page!
+            </v-alert>
           </v-flex>
         </v-layout>
       </v-container>
@@ -15,3 +17,15 @@ export default {
   })
 }
 </script>
+
+<style scoped>
+.mover {
+  z-index: 0;
+  animation: move 4s linear infinite alternate; 
+}
+
+@keyframes move {
+  from { z-index: 0; transform: scale(1); }
+  to { z-index: 4; transform: scale(2.5); }
+}
+</style>
