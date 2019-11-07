6ce3e6f15d1bf4c700b14b8ab83ae5e8b257dd8f
Abelit <ychenid@live.com>
2019-11-08 00:46:17 +0800

    add vue-perfect-scroll to AppLayout component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,20 +14,26 @@
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
-    </v-app-bar> -->
+    </v-app-bar>-->
 
     <!-- <v-content> -->
-      <router-view/>
+    <router-view />
     <!-- </v-content> -->
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
   data: () => ({
     //
-  }),
+  })
 };
 </script>
+
+
+<style>
+html {
+  overflow-y: auto !important;
+}
+</style>
--- frontend/src/views/layout/AppLayout.vue
+++ frontend/src/views/layout/AppLayout.vue
@@ -1,11 +1,15 @@
 <template>
-  <v-app id="inspire">
-    <app-toolbar></app-toolbar>
-    <app-navigation-drawer></app-navigation-drawer>
-    <v-content>
-      <router-view></router-view>
-    </v-content>
-    <app-footer></app-footer>
+  <v-app id="app-content-page">
+    <vue-perfect-scrollbar id="app-content-page--scroll" :settings="scrollSettings">
+      <app-toolbar></app-toolbar>
+      <app-navigation-drawer></app-navigation-drawer>
+
+      <v-content>
+        <router-view></router-view>
+      </v-content>
+
+      <app-footer></app-footer>
+    </vue-perfect-scrollbar>
   </v-app>
 </template>
 
@@ -14,14 +18,29 @@ import AppToolbar from "@/views/layout/AppToolbar";
 import AppNavigationDrawer from "@/views/layout/AppNavigationDrawer";
 import AppFooter from "@/views/layout/AppFooter";
 
+import VuePerfectScrollbar from "vue-perfect-scrollbar";
+
 export default {
   components: {
     AppToolbar,
     AppNavigationDrawer,
-    AppFooter
+    AppFooter,
+    VuePerfectScrollbar
   },
   created() {
     window.getApp = this;
   }
 };
 </script>
+
+
+<style lang="scss">
+#app-content-page {
+  overflow: hidden;
+
+  #app-content-page--scroll {
+    height: 100vh;
+    overflow: auto;
+  }
+}
+</style>
