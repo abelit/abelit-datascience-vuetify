9bfb99d0e89ebf38613f7d77e9be68ae212f28a0
Abelit <ychenid@live.com>
2019-11-07 23:43:45 +0800

    Installed vue-perfect-scrollbar component and add it to AppNavigationDrawer component

--- frontend/src/views/layout/AppNavigationDrawer.vue
+++ frontend/src/views/layout/AppNavigationDrawer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-navigation-drawer v-model="drawer" app clipped class="pr-0">
+  <v-navigation-drawer v-model="drawer" app clipped class="pr-0" id="app-drawer">
     <!-- <v-toolbar app absolute dense v-if="$vuetify.breakpoint.mdAndDown" width="256">
       <v-avatar size="36px" tile>
         <img
@@ -25,37 +25,45 @@
       </span>
     </v-app-bar>
     <!-- <v-divider dark class="my-0" /> -->
-    <v-list dense>
-      <v-list-item v-for="item in items" :key="item.text" link>
-        <v-list-item-action>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-action>
-        <v-list-item-content>
-          <v-list-item-title>{{ item.text }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-subheader class="mt-4 grey--text text--darken-1">SUBSCRIPTIONS</v-subheader>
+    <vue-perfect-scrollbar class="drawer-menu--scroll" :settings="scrollSettings">
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.text" link>
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-subheader class="mt-4 grey--text text--darken-1">SUBSCRIPTIONS</v-subheader>
 
-      <v-list-item class="mt-4" link>
-        <v-list-item-action>
-          <v-icon color="grey darken-1">mdi-plus-circle-outline</v-icon>
-        </v-list-item-action>
-        <v-list-item-title class="grey--text text--darken-1">Browse Channels</v-list-item-title>
-      </v-list-item>
-      <v-list-item link>
-        <v-list-item-action>
-          <v-icon color="grey darken-1">mdi-settings</v-icon>
-        </v-list-item-action>
-        <v-list-item-title class="grey--text text--darken-1">Manage Subscriptions</v-list-item-title>
-      </v-list-item>
-    </v-list>
+        <v-list-item class="mt-4" link>
+          <v-list-item-action>
+            <v-icon color="grey darken-1">mdi-plus-circle-outline</v-icon>
+          </v-list-item-action>
+          <v-list-item-title class="grey--text text--darken-1">Browse Channels</v-list-item-title>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon color="grey darken-1">mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-title class="grey--text text--darken-1">Manage Subscriptions</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </vue-perfect-scrollbar>
   </v-navigation-drawer>
 </template>
 
 <script>
 //Vue兄弟组件使用bus总线设置全局事件通信
 // import dataBus from "@/dataBus.js";
+
+import VuePerfectScrollbar from "vue-perfect-scrollbar";
+
 export default {
+  components: {
+    VuePerfectScrollbar
+  },
   data: () => ({
     drawer: null,
     items: [
@@ -101,8 +109,16 @@ export default {
 </script>
 
 
-<style>
+<style lang="scss">
 #keep .v-navigation-drawer__border {
   display: none;
+}
+#app-drawer {
+  overflow: hidden;
+
+  .drawer-menu--scroll {
+    height: calc(100vh - 48px);
+    overflow: auto;
+  }
 }
 </style>
