866b322835fa01a190ebc03937a435b96c33f8ad
Abelit <ychenid@live.com>
2019-11-12 17:54:21 +0800

    add color  picker

--- frontend/src/views/layout/AppThemeSetting.vue
+++ frontend/src/views/layout/AppThemeSetting.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <v-navigation-drawer v-model="rightDrawer" fixed right temporary hide-overlay>
-      <v-app-bar fixed dense>
+    <v-navigation-drawer v-model="rightDrawer"  app fixed right temporary hide-overlay max-width="160">
+      <v-app-bar  dense>
         <v-icon>settings</v-icon>
         <span class="title ml-3">{{$vuetify.lang.t("$vuetify.setting.settings")}}</span>
       </v-app-bar>
+     <v-container class="pt-0">
+         <v-row align="center" justify="center">
+         <v-col cols="12">
+             <v-color-picker ></v-color-picker>
+         </v-col>
+     </v-row>
+     <v-divider></v-divider>
+     </v-container>
+
     </v-navigation-drawer>
+
     <v-btn
       small
       fab
@@ -25,10 +35,9 @@
 <script>
 export default {
   data: () => ({
-    rightDrawer: false
+    rightDrawer: true
   }),
-  method: {
-  }
+  method: {}
 };
 </script>
 
