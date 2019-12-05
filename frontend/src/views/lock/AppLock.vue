7071ace25e37dacff398cbf19ca25ceb4a2489cd
abelit <ychenid@live.com>
2019-12-05 21:33:44 +0800

    update AppLock component to merge v-text-field and v-btn or v-btn-group

--- frontend/src/views/lock/AppLock.vue
+++ frontend/src/views/lock/AppLock.vue
@@ -1,25 +1,74 @@
 <template>
   <v-container fluid class="pa-0 ma-0">
     <v-img :src="backgroundImage" height="100vh" width="100%">
+      <v-row align="center" justify="center" style="height: 100%"  no-getters>
+        <v-col cols="8" sm="4">
+          <v-form :lazy-validation="lazy" ref="form" v-model="valid">
+              <v-text-field
+                v-model="password"
+                :label="$vuetify.lang.t('$vuetify.form.password')"
+                type="password"
+                prepend-inner-icon="lock"
+                outlined
+                required
+                dense
+                solo
+                flat
+                :rules="rules($vuetify.lang.t('$vuetify.form.password'),24)"
+                :error-messages="errorMessage"
+              >
+              <template v-slot:append-outer>
+                <v-btn-toggle>
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                      <v-btn @click="handleLogin" height="41" dense v-on="on">
+                        <v-icon dark>lock_open</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>{{ $vuetify.lang.t('$vuetify.tooltip.unlock') }}</span>
+                  </v-tooltip>
+
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                      <v-btn @click="handleLogout" height="41" dense v-on="on">
+                        <v-icon dark>exit_to_app</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>{{ $vuetify.lang.t('$vuetify.tooltip.logout') }}</span>
+                  </v-tooltip>
+                </v-btn-toggle>
+                <!-- <v-btn @click="handleLogin" height="40" dense v-on="on">
+                        <v-icon dark>lock_open</v-icon>
+                      </v-btn> -->
+              </template>
+              </v-text-field>
+                
+          </v-form>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-container>
+</template>
+<!--<template>
+  <v-container fluid class="pa-0 ma-0">
+    <v-img :src="backgroundImage" height="100vh" width="100%">
       <v-row align="center" justify="center" style="height: 100%" class="px-5">
         <v-col cols="8" sm="4">
           <v-form :lazy-validation="lazy" ref="form" v-model="valid">
             <v-input>
-<v-text-field
-                  v-model="password"
-                  :label="$vuetify.lang.t('$vuetify.form.password')"
-                  type="password"
-                  prepend-inner-icon="lock"
-                  outlined
-                  required
-                  dense
-                  solo
-                  :rules="rules($vuetify.lang.t('$vuetify.form.password'),24)"
-                  :error-messages="errorMessage"
-                ></v-text-field>
-           <v-input>
-
-
+              <v-text-field
+                v-model="password"
+                :label="$vuetify.lang.t('$vuetify.form.password')"
+                type="password"
+                prepend-inner-icon="lock"
+                outlined
+                required
+                dense
+                solo
+                :rules="rules($vuetify.lang.t('$vuetify.form.password'),24)"
+                :error-messages="errorMessage"
+              ></v-text-field>
+              <v-input>
                 <v-btn-toggle>
                   <v-tooltip bottom>
                     <template v-slot:activator="{ on }">
@@ -39,14 +88,14 @@
                     <span>{{ $vuetify.lang.t('$vuetify.tooltip.logout') }}</span>
                   </v-tooltip>
                 </v-btn-toggle>
-                </v-input>
+              </v-input>
             </v-input>
           </v-form>
         </v-col>
       </v-row>
     </v-img>
   </v-container>
-</template>
+</template>-->
 
 
 <!-- <template>
@@ -156,3 +205,10 @@ export default {
   }
 };
 </script>
+
+
+<style lang="scss">
+ .v-input__append-outer {
+    margin: 0 !important
+  }
+</style>
