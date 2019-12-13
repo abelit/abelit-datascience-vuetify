f7569873aeff6cbceb175330103b53821401b3bf
Abelit <ychenid@live.com>
2019-12-13 21:08:35 +0800

    add grid

--- frontend/src/views/demo/DynamicGrid.vue
+++ frontend/src/views/demo/DynamicGrid.vue
@@ -1,51 +1,18 @@
 <template>
-  <div class="row">
-    <div class="col-8">
-      <h3>Nested draggable</h3>
-      <nested-draggable :tasks="list" />
-    </div>
-
-    <rawDisplayer class="col-3" :value="list" title="List" />
-  </div>
+  <v-container>
+    <v-row class="d-flex">
+      <v-col cols="12" md="4" class="order-3 pa-2">
+        <v-card outlined tile>First flex item</v-card>
+      </v-col>
+      <v-col cols="12" md="4" class="order-2 pa-2">
+        <v-card outlined tile>Second flex item</v-card>
+      </v-col>
+      <v-col cols="12" md="4" class="order-4 pa-2">
+        <v-card outlined tile>Third flex item</v-card>
+      </v-col>
+      <v-col cols="12" md="2" class="order-1 pa-2">
+        <v-card outlined tile>Four flex item</v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
-
-<script>
-import nestedDraggable from "./nested";
-export default {
-  name: "nested-example",
-  display: "Nested",
-  order: 15,
-  components: {
-    nestedDraggable
-  },
-  data() {
-    return {
-      list: [
-        {
-          name: "task 1",
-          tasks: [
-            {
-              name: "task 2",
-              tasks: []
-            }
-          ]
-        },
-        {
-          name: "task 3",
-          tasks: [
-            {
-              name: "task 4",
-              tasks: []
-            }
-          ]
-        },
-        {
-          name: "task 5",
-          tasks: []
-        }
-      ]
-    };
-  }
-};
-</script>
-<style scoped></style>
