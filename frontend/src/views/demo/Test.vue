8758fad1d6ab64dd9a3e86ee2089c99430d18cde
Abelit <ychenid@live.com>
2020-01-09 22:32:58 +0800

    update

--- frontend/src/views/demo/Test.vue
+++ frontend/src/views/demo/Test.vue
@@ -73,308 +73,105 @@
       </v-row>
       <v-row>
         <v-col>
-          <div class="text-center" >
-          
-            <v-chip-group  show-arrows>
+          <div class="text-center">
+            <v-chip-group show-arrows style="vertical-align: middle">
+              <span
+                style="border: 1px orange  solid; height: 32px;line-height: 30px;z-index: 2000;background-color: green"
+                v-if="chip1"
+                class="mx-1"
+              >
+                <v-chip
+                  class="ma-0 pa-0 py-1"
+                  tag
+                  :to="url"
+                  style="background-opacity:.5"
+                  @click:close="closeChip"
+                  @contextmenu.prevent.native="openMenu(tag,$event)"
+                >Remove</v-chip>
+                <span style="z-index: 2000" @click="chip1 = false">
+                  <v-icon small fab>close</v-icon>
+                </span>
+              </span>
+              <span
+                style="border: 1px orange  solid;border-radius: 3px; height:32px;line-height: 30px; text-align:center"
+                v-if="chip1"
+              >
+                <span to="/" class="ma-1" @contextmenu.prevent.native="openMenu(tag,$event)">page</span>
+                <span style="z-index: 2000" @click="chip1 = false" class="ma-1">
+                  <v-icon small>close</v-icon>
+                </span>
+              </span>
+
               <v-chip
-              tag
-              v-if="chip2"
-              class="ma-1"
-              close
-              to="/"
-              color="red"
-              text-color="white"
-              @click:close="closeChip"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Remove</v-chip>
+                tag
+                v-if="chip2"
+                class="ma-1"
+                close
+                :to="url"
+                color="red"
+                text-color="white"
+                @click:close="closeChip"
+                style="z-index: 2000"
+                @contextmenu.prevent.native="openMenu(tag,$event)"
+              >Remove</v-chip>
 
-            <v-chip
-              tag
-              v-if="chip3"
-              class="ma-1"
-              close
-              color="green"
-              outlined
-              to="/"
-              @click:close="chip3 = false"
-            >Success</v-chip>
+              <v-chip
+                tag
+                v-if="chip3"
+                class="ma-1"
+                close
+                :to="url"
+                color="green"
+                outlined
+                @click:close="chip3 = false"
+              >Success</v-chip>
 
-            <v-chip
-              v-if="chip4"
-              class="ma-1"
-              close
-              color="orange"
-              label
-              outlined
-              @click:close="chip4 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Complete</v-chip>
-            <v-chip tag v-if="chip1" class="ma-1" close @click:close="chip1 = false"
-            @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Closable</v-chip>
+              <v-chip
+                v-if="chip4"
+                class="ma-1"
+                close
+                :to="url"
+                color="orange"
+                label
+                outlined
+                @click:close="chip4 = false"
+                @contextmenu.prevent.native="openMenu(tag,$event)"
+              >Complete</v-chip>
+              <v-chip
+                tag
+                v-if="chip1"
+                class="ma-1"
+                :to="url"
+                close
+                @click(close).prevent.stop="chip1 = false"
+                @contextmenu.prevent.native="openMenu(tag,$event)"
+              >Closable</v-chip>
 
-            <v-chip
-              tag
-              v-if="chip2"
-              class="ma-1"
-              close
-              to="/"
-              color="red"
-              text-color="white"
-              @click:close.prevent.stop="chip2 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Remove</v-chip>
-
-            <v-chip
-              tag
-              v-if="chip3"
-              class="ma-1"
-              close
-              color="green"
-              outlined
-              @click:close="chip3 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Success</v-chip>
-
-            <v-chip
-              v-if="chip4"
-              class="ma-1"
-              close
-              color="orange"
-              label
-              outlined
-              @click:close="chip4 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Complete</v-chip>
-            <v-chip tag v-if="chip1" class="ma-1" close @click:close="chip1 = false"
-            @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Closable</v-chip>
-
-            <v-chip
-              tag
-              v-if="chip2"
-              class="ma-1"
-              close
-              to="/"
-              color="red"
-              text-color="white"
-              @click:close.prevent.stop="chip2 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Remove</v-chip>
-
-            <v-chip
-              tag
-              v-if="chip3"
-              class="ma-1"
-              close
-              color="green"
-              outlined
-              @click:close="chip3 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Success</v-chip>
-
-            <v-chip
-              v-if="chip4"
-              class="ma-1"
-              close
-              color="orange"
-              label
-              outlined
-              @click:close="chip4 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Complete</v-chip>
-            <v-chip tag v-if="chip1" class="ma-1" close @click:close="chip1 = false"
-            @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Closable</v-chip>
-
-            <v-chip
-              tag
-              v-if="chip2"
-              class="ma-1"
-              close
-              to="/"
-              color="red"
-              text-color="white"
-              @click:close.prevent.stop="chip2 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Remove</v-chip>
-
-            <v-chip
-              tag
-              v-if="chip3"
-              class="ma-1"
-              close
-              color="green"
-              outlined
-              @click:close="chip3 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Success</v-chip>
-
-            <v-chip
-              v-if="chip4"
-              class="ma-1"
-              close
-              color="orange"
-              label
-              outlined
-              @click:close="chip4 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Complete</v-chip>
-            <v-chip tag v-if="chip1" class="ma-1" close @click:close="chip1 = false"
-            @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Closable</v-chip>
-
-            <v-chip
-              tag
-              v-if="chip2"
-              class="ma-1"
-              close
-              to="/"
-              color="red"
-              text-color="white"
-              @click:close.prevent.stop="chip2 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Remove</v-chip>
-
-            <v-chip
-              tag
-              v-if="chip3"
-              class="ma-1"
-              close
-              color="green"
-              outlined
-              @click:close="chip3 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Success</v-chip>
-
-            <v-chip
-              v-if="chip4"
-              class="ma-1"
-              close
-              color="orange"
-              label
-              outlined
-              @click:close="chip4 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Complete</v-chip>
-            <v-chip tag v-if="chip1" class="ma-1" close @click:close="chip1 = false"
-            @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Closable</v-chip>
-
-            <v-chip
-              tag
-              v-if="chip2"
-              class="ma-1"
-              close
-              to="/"
-              color="red"
-              text-color="white"
-              @click:close.prevent.stop="chip2 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Remove</v-chip>
-
-            <v-chip
-              tag
-              v-if="chip3"
-              class="ma-1"
-              close
-              color="green"
-              outlined
-              @click:close="chip3 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Success</v-chip>
-
-            <v-chip
-              v-if="chip4"
-              class="ma-1"
-              close
-              color="orange"
-              label
-              outlined
-              @click:close="chip4 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Complete</v-chip>
-            <v-chip tag v-if="chip1" class="ma-1" close @click:close="chip1 = false"
-            @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Closable</v-chip>
-
-            <v-chip
-              tag
-              v-if="chip2"
-              class="ma-1"
-              close
-              to="/"
-              color="red"
-              text-color="white"
-              @click:close.prevent.stop="chip2 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Remove</v-chip>
-
-            <v-chip
-              tag
-              v-if="chip3"
-              class="ma-1"
-              close
-              color="green"
-              outlined
-              @click:close="chip3 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Success</v-chip>
-
-            <v-chip
-              v-if="chip4"
-              class="ma-1"
-              close
-              color="orange"
-              label
-              outlined
-              @click:close="chip4 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Complete</v-chip>
-            <v-chip tag v-if="chip1" class="ma-1" close @click:close="chip1 = false"
-            @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Closable</v-chip>
-
-            <v-chip
-              tag
-              v-if="chip2"
-              class="ma-1"
-              close
-              to="/"
-              color="red"
-              text-color="white"
-              @click:close.prevent.stop="chip2 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Remove</v-chip>
-
-            <v-chip
-              tag
-              v-if="chip3"
-              class="ma-1"
-              close
-              color="green"
-              outlined
-              @click:close="chip3 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Success</v-chip>
-
-            <v-chip
-              v-if="chip4"
-              class="ma-1"
-              close
-              color="orange"
-              label
-              outlined
-              @click:close="chip4 = false"
-              @contextmenu.prevent.native="openMenu(tag,$event)"
-            >Complete</v-chip>
+              <router-link
+                ref="tag"
+                :to="url"
+                tag="span"
+                class="tags-view-item"
+                style="border: 1px orange  solid;border-radius: 3px; height:32px;line-height: 30px; text-align:center"
+                v-if="chip1"
+              >
+                ceshi
+                <span @click.prevent.stop="chip1 = false" class="ma-1">
+                  <v-icon small>close</v-icon>
+                </span>
+              </router-link>
             </v-chip-group>
           </div>
-          <ul v-show="visible"  :style="{left:left+'px',top:top+'px'}"  class="contextmenuaa" slot="mytag">
-            <li >Refresh</li>
-            <li >Close</li>
-            <li >Close Others</li>
-            <li >Close All</li>
+          <ul
+            v-show="visible"
+            :style="{left:left+'px',top:top+'px'}"
+            class="contextmenuaa"
+            slot="mytag"
+          >
+            <li>Refresh</li>
+            <li>Close</li>
+            <li>Close Others</li>
+            <li>Close All</li>
           </ul>
         </v-col>
       </v-row>
@@ -392,18 +189,23 @@ export default {
     chip2: true,
     chip3: true,
     chip4: true,
-     visible: false,
-      top: 0,
-      left: 0,
-      selectedTag: {},
-      affixTags: []
+    visible: false,
+    top: 0,
+    left: 0,
+    selectedTag: {},
+    affixTags: [],
+    url: "/"
   }),
 
   methods: {
     closeChip() {
       console.log("hi");
+      this.url = "#";
+      console.log(this.$router);
       alert("hi");
-      return false;
+
+      this.chip2 = false;
+      return;
     },
     clickMe() {
       this.loading = true;
@@ -414,37 +216,43 @@ export default {
       }, 2000);
     },
     openMenu(tag, e) {
-      const menuMinWidth = 105
-      const offsetLeft = this.$el.getBoundingClientRect().left // container margin left
-      const offsetWidth = this.$el.offsetWidth // container width
-      const maxLeft = offsetWidth - menuMinWidth // left boundary
-      const left = e.clientX - offsetLeft + 15 // 15: margin right
+      const menuMinWidth = 105;
+      const offsetLeft = this.$el.getBoundingClientRect().left; // container margin left
+      const offsetWidth = this.$el.offsetWidth; // container width
+      const maxLeft = offsetWidth - menuMinWidth; // left boundary
+      const left = e.clientX - offsetLeft + 15; // 15: margin right
       if (left > maxLeft) {
-        this.left = maxLeft
+        this.left = maxLeft;
       } else {
-        this.left = left
+        this.left = left;
       }
-      this.top = e.clientY
-      this.visible = true
-      this.selectedTag = tag
+      this.top = e.clientY;
+      this.visible = true;
+      this.selectedTag = tag;
     },
     closeMenu() {
-      this.visible = false
+      this.visible = false;
     }
   },
   watch: {
     visible(value) {
       if (value) {
-        document.body.addEventListener('click', this.closeMenu)
+        document.body.addEventListener("click", this.closeMenu);
       } else {
-        document.body.removeEventListener('click', this.closeMenu)
+        document.body.removeEventListener("click", this.closeMenu);
       }
     }
+  },
+  mounted() {
+    console.log("v-chip mounted");
   }
 };
 </script>
 
 <style lang="scss">
+.v-chip__content {
+  // background-color: #fff;
+}
 .v-input__append-outer {
   margin: 0 0 !important;
 }
@@ -452,24 +260,24 @@ export default {
   margin: 0 0 !important;
 }
 .contextmenuaa {
+  margin: 0;
+  background: #fff;
+  z-index: 3000;
+  position: absolute;
+  list-style-type: none;
+  padding: 5px 0;
+  border-radius: 4px;
+  font-size: 12px;
+  font-weight: 400;
+  color: #333;
+  box-shadow: 2px 2px 3px 0 rgba(0, 0, 0, 0.3);
+  li {
     margin: 0;
-    background: #fff;
-    z-index: 3000;
-    position: absolute;
-    list-style-type: none;
-    padding: 5px 0;
-    border-radius: 4px;
-    font-size: 12px;
-    font-weight: 400;
-    color: #333;
-    box-shadow: 2px 2px 3px 0 rgba(0, 0, 0, .3);
-    li {
-      margin: 0;
-      padding: 7px 16px;
-      cursor: pointer;
-      &:hover {
-        background: #eee;
-      }
+    padding: 7px 16px;
+    cursor: pointer;
+    &:hover {
+      background: #eee;
     }
   }
+}
 </style>
